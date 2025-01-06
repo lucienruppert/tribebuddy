59ffbb1e536df885d17f10c31d7577b7c166bf84
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-01-06 22:14:44 +0100

    update

---
+++ src/app/material.module.js
@@ -0,0 +1,81 @@
+"use strict";
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
+    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
+    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MaterialModules = void 0;
+const core_1 = require("@angular/core");
+const menu_1 = require("@angular/material/menu");
+const icon_1 = require("@angular/material/icon");
+const button_1 = require("@angular/material/button");
+const sidenav_1 = require("@angular/material/sidenav");
+const list_1 = require("@angular/material/list");
+const toolbar_1 = require("@angular/material/toolbar");
+const toolbar_2 = require("@angular/material/toolbar");
+const MaterialComponents = [
+    menu_1.MatMenuModule,
+    icon_1.MatIconModule,
+    button_1.MatButtonModule,
+    sidenav_1.MatSidenavModule,
+    button_1.MatButtonModule,
+    icon_1.MatIconModule,
+    list_1.MatNavList,
+    toolbar_1.MatToolbar,
+    toolbar_2.MatToolbarRow,
+];
+let MaterialModules = (() => {
+    let _classDecorators = [(0, core_1.NgModule)({
+            imports: [MaterialComponents],
+            exports: [MaterialComponents],
+        })];
+    let _classDescriptor;
+    let _classExtraInitializers = [];
+    let _classThis;
+    var MaterialModules = _classThis = class {
+    };
+    __setFunctionName(_classThis, "MaterialModules");
+    (() => {
+        const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
+        MaterialModules = _classThis = _classDescriptor.value;
+        if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        __runInitializers(_classThis, _classExtraInitializers);
+    })();
+    return MaterialModules = _classThis;
+})();
+exports.MaterialModules = MaterialModules;
